56c67d9e12090eb4e298bd687fc7aecc8c8ec936
Peter Pesotskiy <ppeter777@yandex.ru>
2023-05-13 02:12:14 +0300

    modify Differ.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,46 +1,40 @@
 package hexlet.code;
 
-//import java.nio.file.Files;
-//import java.nio.file.Path;
-//import java.nio.file.Paths;
-
-//import java.io.File;
-//import java.util.*;
-
-//import picocli.CommandLine;
-//import picocli.CommandLine.Command;
-//import picocli.CommandLine.Option;
-//import picocli.CommandLine.Parameters;
-
-
-
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class Differ {
-    public static String generate(String filePath1, String filePath2) {
-        return null;
-    }
     public static String genDiff(Map mapFile1, Map mapFile2) {
         Set<String> keys = new TreeSet<>();
         keys.addAll(mapFile1.keySet());
         keys.addAll(mapFile2.keySet());
 
-//        System.out.println("keys:\n" + keys + "\n");
         StringBuilder stringBuilder = new StringBuilder().append("{\n");
         for (var key: keys) {
+            var value1 = mapFile1.get(key);
+            var value2 = mapFile2.get(key);
+            String value1String;
+            String value2String;
+            
+            if (value1 == null) {
+                value1String = "null";
+            } else value1String = mapFile1.get(key).toString();
+            if (value2 == null) {
+                value2String = "null";
+            } else value2String = mapFile2.get(key).toString();
+
             if (mapFile1.containsKey(key) && mapFile2.containsKey(key)) {
-                if (mapFile1.get(key).equals(mapFile2.get(key))) {
-                    stringBuilder.append("   ").append(key).append(": ").append(mapFile1.get(key)).append("\n");
+                if (value1String.equals(value2String)) {
+                    stringBuilder.append("   ").append(key).append(": ").append(value1String).append("\n");
                 } else {
-                    stringBuilder.append(" - ").append(key).append(": ").append(mapFile1.get(key)).append("\n");
-                    stringBuilder.append(" + ").append(key).append(": ").append(mapFile2.get(key)).append("\n");
+                    stringBuilder.append(" - ").append(key).append(": ").append(value1String).append("\n");
+                    stringBuilder.append(" + ").append(key).append(": ").append(value2String).append("\n");
                 }
             } else if (mapFile1.containsKey(key)) {
-                stringBuilder.append(" - ").append(key).append(": ").append(mapFile1.get(key)).append("\n");
+                stringBuilder.append(" - ").append(key).append(": ").append(value1String).append("\n");
             } else {
-                stringBuilder.append(" + ").append(key).append(": ").append(mapFile2.get(key)).append("\n");
+                stringBuilder.append(" + ").append(key).append(": ").append(value2String).append("\n");
             }
         }
         stringBuilder.append("}");
