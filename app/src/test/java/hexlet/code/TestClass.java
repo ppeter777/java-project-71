171f288fae57ca81bd6f69593871b84ac4bf7a3a
Peter Pesotskiy <ppeter777@yandex.ru>
2023-05-19 01:03:18 +0300

    linter correction

--- app/src/test/java/hexlet/code/TestClass.java
+++ app/src/test/java/hexlet/code/TestClass.java
@@ -17,19 +17,19 @@ public class TestClass {
         File file1 = new File("src/test/resources/file1Nested.json");
         File file2 = new File("src/test/resources/file2Nested.json");
         String expected = """
-        Property 'chars2' was updated. From [complex value] to false
-        Property 'checked' was updated. From false to true
-        Property 'default' was updated. From null to [complex value]
-        Property 'id' was updated. From 45 to null
-        Property 'key1' was removed
-        Property 'key2' was added with value: value2
-        Property 'numbers2' was updated. From [complex value] to [complex value]
-        Property 'numbers3' was removed
-        Property 'numbers4' was added with value: [complex value]
-        Property 'obj1' was added with value: [complex value]
-        Property 'setting1' was updated. From Some value to Another value
-        Property 'setting2' was updated. From 200 to 300
-        Property 'setting3' was updated. From true to none
+            Property 'chars2' was updated. From [complex value] to false
+            Property 'checked' was updated. From false to true
+            Property 'default' was updated. From null to [complex value]
+            Property 'id' was updated. From 45 to null
+            Property 'key1' was removed
+            Property 'key2' was added with value: value2
+            Property 'numbers2' was updated. From [complex value] to [complex value]
+            Property 'numbers3' was removed
+            Property 'numbers4' was added with value: [complex value]
+            Property 'obj1' was added with value: [complex value]
+            Property 'setting1' was updated. From Some value to Another value
+            Property 'setting2' was updated. From 200 to 300
+            Property 'setting3' was updated. From true to none
         """;
         var actual = Differ.generate(file1, file2, "plain");
         assertEquals(expected, actual);
