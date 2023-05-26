f0688ec8ef225dcea6a2873384707320795032e3
Peter Pesotskiy <ppeter777@yandex.ru>
2023-05-26 23:07:59 +0300

    correct tests

--- app/src/test/java/hexlet/code/TestClass.java
+++ app/src/test/java/hexlet/code/TestClass.java
@@ -57,29 +57,29 @@ public class TestClass {
         String filepath2 = "src/test/resources/file2Nested.json";
         String expected = """
                 {
-                   chars1: [a, b, c]
-                 - chars2: [d, e, f]
-                 + chars2: false
-                 - checked: false
-                 + checked: true
-                 - default: null
-                 + default: [value1, value2]
-                 - id: 45
-                 + id: null
-                 - key1: value1
-                 + key2: value2
-                   numbers1: [1, 2, 3, 4]
-                 - numbers2: [2, 3, 4, 5]
-                 + numbers2: [22, 33, 44, 55]
-                 - numbers3: [3, 4, 5]
-                 + numbers4: [4, 5, 6]
-                 + obj1: {nestedKey=value, isNested=true}
-                 - setting1: Some value
-                 + setting1: Another value
-                 - setting2: 200
-                 + setting2: 300
-                 - setting3: true
-                 + setting3: none
+                    chars1: [a, b, c]
+                  - chars2: [d, e, f]
+                  + chars2: false
+                  - checked: false
+                  + checked: true
+                  - default: null
+                  + default: [value1, value2]
+                  - id: 45
+                  + id: null
+                  - key1: value1
+                  + key2: value2
+                    numbers1: [1, 2, 3, 4]
+                  - numbers2: [2, 3, 4, 5]
+                  + numbers2: [22, 33, 44, 55]
+                  - numbers3: [3, 4, 5]
+                  + numbers4: [4, 5, 6]
+                  + obj1: {nestedKey=value, isNested=true}
+                  - setting1: Some value
+                  + setting1: Another value
+                  - setting2: 200
+                  + setting2: 300
+                  - setting3: true
+                  + setting3: none
                 }""";
         var actual = Differ.generate(filepath1, filepath2, "stylish");
         assertEquals(expected, actual);
@@ -91,29 +91,29 @@ public class TestClass {
         String filepath2 = "src/test/resources/file2Nested.yaml";
         String expected = """
                 {
-                   chars1: [a, b, c]
-                 - chars2: [d, e, f]
-                 + chars2: false
-                 - checked: false
-                 + checked: true
-                 - default: null
-                 + default: [value1, value2]
-                 - id: 45
-                 + id: null
-                 - key1: value1
-                 + key2: value2
-                   numbers1: [1, 2, 3, 4]
-                 - numbers2: [2, 3, 4, 5]
-                 + numbers2: [22, 33, 44, 55]
-                 - numbers3: [3, 4, 5]
-                 + numbers4: [4, 5, 6]
-                 + obj1: {nestedKey=value, isNested=true}
-                 - setting1: Some value
-                 + setting1: Another value
-                 - setting2: 200
-                 + setting2: 300
-                 - setting3: true
-                 + setting3: none
+                    chars1: [a, b, c]
+                  - chars2: [d, e, f]
+                  + chars2: false
+                  - checked: false
+                  + checked: true
+                  - default: null
+                  + default: [value1, value2]
+                  - id: 45
+                  + id: null
+                  - key1: value1
+                  + key2: value2
+                    numbers1: [1, 2, 3, 4]
+                  - numbers2: [2, 3, 4, 5]
+                  + numbers2: [22, 33, 44, 55]
+                  - numbers3: [3, 4, 5]
+                  + numbers4: [4, 5, 6]
+                  + obj1: {nestedKey=value, isNested=true}
+                  - setting1: Some value
+                  + setting1: Another value
+                  - setting2: 200
+                  + setting2: 300
+                  - setting3: true
+                  + setting3: none
                 }""";
         var actual = Differ.generate(filepath1, filepath2, "stylish");
         assertEquals(expected, actual);
@@ -149,7 +149,7 @@ public class TestClass {
         Map<String, Object> input2 = new HashMap<>();
         input2.put("key1", "1");
         String actual = Stylish.formatStylish(Differ.genDiff(input1, input2));
-        String expected = "{\n   key1: 1\n}";
+        String expected = "{\n    key1: 1\n}";
         assertEquals(expected, actual);
     }
 
@@ -160,7 +160,7 @@ public class TestClass {
         Map<String, Object> input2 = new HashMap<>();
         input2.put("key1", "11");
         String actual = Stylish.formatStylish(Differ.genDiff(input1, input2));
-        String expected = "{\n - key1: 1\n + key1: 11\n}";
+        String expected = "{\n  - key1: 1\n  + key1: 11\n}";
         assertEquals(expected, actual);
     }
     @Test
@@ -171,7 +171,7 @@ public class TestClass {
         input2.put("key1", "1");
         input2.put("key3", "3");
         String actual = Stylish.formatStylish(Differ.genDiff(input1, input2));
-        String expected = "{\n   key1: 1\n + key3: 3\n}";
+        String expected = "{\n    key1: 1\n  + key3: 3\n}";
         assertEquals(expected, actual);
     }
 
@@ -183,7 +183,7 @@ public class TestClass {
         Map<String, Object> input2 = new HashMap<>();
         input2.put("key2", "2");
         String actual = Stylish.formatStylish(Differ.genDiff(input1, input2));
-        String expected = "{\n - key1: 1\n   key2: 2\n}";
+        String expected = "{\n  - key1: 1\n    key2: 2\n}";
         assertEquals(expected, actual);
     }
 }
